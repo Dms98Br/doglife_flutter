13f82492a483fb460cf209456c9a516d18a4ec5e
Daniel Moya da Silva <danielm.silva@univem.edu.br>
2021-03-14 21:02:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# doglife_flutter
+ video aula de criação de template no flutter
---
+++ android/app/src/main/kotlin/com/example/doglife_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doglife_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
